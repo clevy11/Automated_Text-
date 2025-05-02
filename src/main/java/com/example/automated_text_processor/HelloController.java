d2d676c937f08fab7ee0189e88ee27249eed29bf
clb <buntulevycaleb@gmail.com>
2025-05-02 13:25:33 +0200

    the initial commit

---
+++ src/main/java/com/example/automated_text_processor/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.automated_text_processor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
